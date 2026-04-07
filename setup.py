d7ba7ad28f7ba76a3f5741a4f01b5baebb5794b9
Maslyata <ilyasik0389@gmail.com>
2026-04-07 13:02:55 +0300

    Добавлен первый узел: вывод Hello ROS 2 World

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'maslo_from_1406'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='igsp-01',
-    maintainer_email='igsp-01@todo.todo',
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    license='TODO: License',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'first_node = maslo_from_1406.first_node:main',   # ← добавляем эту строку
         ],
     },
 )
+
